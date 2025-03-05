e360d87b236b4e2eec10c463edc51ae4ec1ed714
Samuel Mariano Melo <89424721+Sam1536@users.noreply.github.com>
2025-03-05 15:37:58 -0300

    turrent tank

--- Assets/Scripts/Tank/TankTurret.cs
+++ Assets/Scripts/Tank/TankTurret.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class TankTurret : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public float rotationSpeed = 5.0f;
+
+    public Transform turrent;
+
+    private RaycastHit hit;
+
+    
+
+    
+    void FixedUpdate()
     {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (!Physics.Raycast(ray, out hit))
+            return;
         
+
+        Vector3 point = hit.point - turrent.position;
+        point.y = 0;
+
+        Quaternion toRotation = Quaternion.LookRotation(point);
+
+        turrent.rotation = Quaternion.Lerp(turrent.rotation, toRotation, rotationSpeed * Time.deltaTime);
     }
 }
