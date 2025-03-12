a7a6f6d69f21a61396502e4c2190773984090389
Samuel Mariano Melo <89424721+Sam1536@users.noreply.github.com>
2025-03-12 19:26:44 -0300

    Programming the life system

--- Assets/Scripts/Turret/TurretHealth.cs
+++ Assets/Scripts/Turret/TurretHealth.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TurretHealth : MonoBehaviour
 {
+
+    public float life = 100f;
+    
+    public float minDamage = 15f;
+    public float maxDamage = 30f;
+
+    public Image healthBar;
+
+    private TurretController turretController;
+
     // Start is called before the first frame update
     void Start()
     {
+        ChangerHealthBar();
         
+        turretController = GetComponent<TurretController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!turretController.control)
+            return;
+
         
+    }
+
+
+    private void ChangerHealthBar()
+    {
+        healthBar.fillAmount = life / 100;
+
+    }
+
+    private void SetDamage()
+    {
+        ChangerHealthBar();
+
+        life -= Random.Range(minDamage, maxDamage);
+
+        if(life <= 0)
+        {
+            turretController.control = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            SetDamage();
+            //Debug.Log("PEGOU");
+        }
     }
 }
