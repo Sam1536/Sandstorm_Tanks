a548f58262ec8fb803eec98a0636cd194911ee06
Samuel Mariano Melo <89424721+Sam1536@users.noreply.github.com>
2025-03-15 17:17:10 -0300

    Creating the GameOver screen

--- Assets/Scripts/UI/CanvaMainGame.cs
+++ Assets/Scripts/UI/CanvaMainGame.cs
@@ -9,9 +9,18 @@ public class CanvaMainGame : MonoBehaviour
     [Header("PLayer Health")]
     public Image healthBar;
 
+    [Tooltip("Qual é a cena a ser carregada quando clicar no botão Reset?")]
+    public string sceneGame = "MainGame";
+
     public void BackToManu()
     {
         SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ResetGame()
+    {
+        SceneManager.LoadScene(sceneGame);
+
     }
 
     public void HealthValue(float health)
